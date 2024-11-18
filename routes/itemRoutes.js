5560f6ba197c846e6f3c86c1c819eba78c0245b2
lightquin <jqn3nidad@gmail.com>
2024-11-18 20:35:53 +0800

    placeholder code for itemRoutes

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const itemController = require('../controllers/itemController');
 
+router.get('/', itemController.getAllItems);
+router.post('/', itemController.createItem);
+router.put('/:id', itemController.updateItem);
+router.patch('/:id', itemController.partialUpdateItem);
+router.delete('/:id', itemController.deleteItem);
+
+module.exports = router;
