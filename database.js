570dba0280d14615c28330fec963cc60dec3355f
lightquin <jqn3nidad@gmail.com>
2024-11-18 20:34:46 +0800

    initial code items

--- database.js
+++ database.js
@@ -1,1 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
+const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'), (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    db.run(`CREATE TABLE IF NOT EXISTS items (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT,
+      date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
+  }
+});
+
+module.exports = db;
