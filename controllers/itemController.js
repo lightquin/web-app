5953a85aef67d5cdd5af55ef3e6f04853f49c11a
lightquin <jqn3nidad@gmail.com>
2024-11-18 20:36:29 +0800

    placeholder code will fix up later

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,1 +1,24 @@
+const db = require('../database');
+
+exports.getAllItems = (req, res) => {
+  db.all('SELECT * FROM items', [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.render('items', { items: rows });
+  });
+};
+
+exports.createItem = (req, res) => {
+  const { name, description } = req.body;
+  db.run('INSERT INTO items (name, description) VALUES (?, ?)', [name, description], function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.redirect('/items');
+  });
+};
+
 
