d5c5e8d700ea96933d9713c3097cedbd770df1ce
lightquin <jqn3nidad@gmail.com>
2024-11-29 11:54:54 +0800

    Update item.js

--- models/item.js
+++ models/item.js
@@ -1,17 +1,15 @@
-// models/item.js
-const mongoose = require('mongoose');
+// item.js
 
-const itemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    // Add other fields as necessary
-});
+class Item {
+    constructor(name, price, description) {
+        this.name = name;
+        this.price = price;
+        this.description = description || '';
+    }
+}
 
-const Item = mongoose.model('Item', itemSchema);
-module.exports = Item;
+// Example of creating an item
+const exampleItem = new Item('Sample Item', 19.99, 'This is a sample item description.');
+
+// Export the Item class or the example item
+module.exports = Item; // or module.exports = exampleItem; if you want to export a specific instance
