91f9171309b3c3e6dd3de0fef7b743bba58c8383
lightquin <jqn3nidad@gmail.com>
2024-11-29 11:39:32 +0800

    Create item.js

---
+++ models/item.js
@@ -0,0 +1,17 @@
+// models/item.js
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    // Add other fields as necessary
+});
+
+const Item = mongoose.model('Item', itemSchema);
+module.exports = Item;
